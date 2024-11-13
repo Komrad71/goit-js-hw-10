1c0bd23f833f8310a1384ebb59720887dd145750
Komrad71 <an.vasichkin@gmail>
2024-11-14 01:45:08 +0200

    timer Done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,19 +1,26 @@
 // Описаний в документації
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
 // npm i flatpickr --save     - Встановлення через NPM
 
 // Описаний у документації
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 
 // npm install izitoast --save
 
+const startBtn = document.querySelector('button[data-start]');
+const datePick = document.querySelector('#datetime-picker');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
 
-
+let userSelectedDate = null;
+let timerInterval = null;
 
 const options = {
   enableTime: true,
@@ -21,9 +28,26 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      iziToast.error({
+        title: 'Error',
+        message: '❌ Please choose a date in the future',
+        color: 'red',
+        position: 'topCenter',
+      });
+      startBtn.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startBtn.disabled = false;
+    }
   },
 };
+
+flatpickr(datePick, options);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -44,7 +68,49 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+/* console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20} */
 
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  daysValue.textContent = addLeadingZero(days);
+  hoursValue.textContent = addLeadingZero(hours);
+  minutesValue.textContent = addLeadingZero(minutes);
+  secondsValue.textContent = addLeadingZero(seconds);
+}
+
+function startTimer() {
+  const interval = 1000;
+
+  timerInterval = setInterval(() => {
+    const currentTime = new Date();
+    const remainingTime = userSelectedDate - currentTime;
+
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      updateTimerDisplay(convertMs(0));
+      iziToast.success({
+        title: 'Timer ended',
+        message: 'Countdown finished!',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+      datePick.disabled = false;
+      return;
+    }
+
+    const timeValues = convertMs(remainingTime);
+    updateTimerDisplay(timeValues);
+  }, interval);
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  datePick.disabled = true;
+  startTimer();
+});
+
+// форматування значення таймера в інтерфейсі ( якщо в числі менше двох символів, то додає 0)
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
